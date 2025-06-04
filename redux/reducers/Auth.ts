355175731157a268fc5a01fb9d3ce8f0a9fc80f2
Eren AVAR <eren.avar@hotmail.com>
2025-06-05 00:37:03 +0300

    complete type assigns for auth.ts

--- redux/reducers/Auth.ts
+++ redux/reducers/Auth.ts
@@ -1,13 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { UserType, AuthState } from "../types/Auth";
 
+const initialState: AuthState = {
+  authenticated: false,
+  UserType: UserType.COLLECTOR,
+  username: "",
+};
+
 const authSlice = createSlice({
   name: "auth",
-  initialState: (AuthState = {
-    authenticated: false,
-    UserType: UserType.COLLECTOR,
-    username: "",
-  }),
+  initialState,
   reducers: {
     setAuthenticated: (state, action: PayloadAction<boolean>) => {
       state.authenticated = action.payload;
