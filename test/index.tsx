84f9b0564e4da2dab31cbfeb2b99e8cf25b434d5
Eren AVAR <eren.avar@hotmail.com>
2025-06-06 01:32:51 +0300

    set up testing environment

---
+++ test/index.tsx
@@ -0,0 +1,18 @@
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { render, RenderOptions } from "@testing-library/react-native";
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>{children}</NavigationContainer>
+    </Provider>
+  );
+};
+
+const customRender = (ui: React.ReactElement, options?: RenderOptions) => {
+  render(ui, { wrapper: AllTheProviders, ...options });
+};
+export * from "@testing-library/react-native";
+export { customRender as render };
