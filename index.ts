9c26f4ebbb8dc191f2f28063ae28a44681593577
Eren AVAR <eren.avar@hotmail.com>
2025-06-16 23:10:20 +0300

    install firebase

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { registerRootComponent } from "expo";
-import App from "./App"; // App.tsx dosyanızın yolu bu olmalı. Eğer App.tsx başka bir dizindeyse yolu güncelleyin (örn: './src/App').
+import App from "./App";
 
-// Uygulamanızın ana bileşenini Expo'ya kaydeder
 registerRootComponent(App);
