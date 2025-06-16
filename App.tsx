5e7b6eaa7a2cdc278b3fd14bd6274f5c4a69505d
Eren AVAR <eren.avar@hotmail.com>
2025-06-16 21:39:17 +0300

    add clerks dependencies and create .env for clerk

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { Provider } from "react-redux";
 import MainNavigation from "./src/navigation/MainNavigation";
 import store from "./redux/store";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <MainNavigation />
-    </Provider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Provider store={store}>
+        <MainNavigation />
+      </Provider>
+    </ClerkProvider>
   );
 }
