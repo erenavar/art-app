a11f38940106a8b5941ef8104f1d21ebe54bd5b8
Eren AVAR <eren.avar@hotmail.com>
2025-06-22 21:47:09 +0300

    add sign up functions usong clerk

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,4 @@
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useRef, useState } from "react";
 import {
   Poppins_300Light,
@@ -16,12 +10,14 @@ import Entypo from "react-native-vector-icons/Entypo";
 import CustomTextInput from "./CustomTextInput";
 import { LinearGradient } from "expo-linear-gradient";
 import VerificationArea from "./VerificationArea";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const Form = () => {
   const [loaded, error] = useFonts({
     Poppins_300Light,
     Poppins_400Regular,
   });
+  const { isLoaded, signUp, setActive } = useSignUp();
   const [form, setForm] = useState<IFormData>({
     email: "",
     fullName: "",
@@ -50,13 +46,44 @@ const Form = () => {
     }
     if (form.password.length < 8) {
       setPasswordInputError(
-        "Password must be longer than 7 charactaters. Please review it."
+        "Password must be at least 8 characters. Please review it."
       );
+      valid = false;
     }
+    return valid;
   }
 
-  if (!loaded) return null;
-  if (error) return <Text>Font Error!</Text>;
+  const signUpFunc = async () => {
+    if (!isChecked) {
+      alert("You must agree to Privacy Policy ");
+      return;
+    }
+    if (!validateData) {
+      return;
+    }
+    if (!loaded) {
+      return;
+    }
+
+    try {
+      await signUp!.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+      await signUp?.prepareEmailAddressVerification({ strategy: "email_code" });
+      setPendingVerification(true);
+    } catch (error: unknown) {
+      console.log("error on form component", error);
+    }
+  };
+
+  const onPressVerify = async () => {
+    const completeSignUp = await signUp?.attemptEmailAddressVerification({
+      code: code.join(""),
+    });
+  };
+
+  if (!loaded || error) return <></>;
 
   return (
     <View style={styles.container}>
